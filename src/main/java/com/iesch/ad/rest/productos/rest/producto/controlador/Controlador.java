f2f62dbe08e4fd8faf578e817aa48006d7c33e28
dam2 <mrubios>
2023-01-27 20:23:11 +0100

    ResponseEntity en los GET

--- src/main/java/com/iesch/ad/rest/productos/rest/producto/controlador/Controlador.java
+++ src/main/java/com/iesch/ad/rest/productos/rest/producto/controlador/Controlador.java
@@ -2,6 +2,7 @@ package com.iesch.ad.rest.productos.rest.producto.controlador;
 
 import com.iesch.ad.rest.productos.rest.producto.modelos.Producto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.iesch.ad.rest.productos.rest.producto.repositorio.ProductoRepositorio;
 
@@ -12,14 +13,22 @@ public class Controlador {
     @Autowired
     ProductoRepositorio productoRepositorio;
 
+    //Error 404 si no hay productos y un 200 si hay uno o más
+    //ResponseEntity devolverá un dato numérico, al ser un ? puede ser null
      @GetMapping("api/producto")
-    public List<Producto> obtenerTodos(){
-         return productoRepositorio.findAll();
+    public ResponseEntity<?> obtenerTodos(){
+         List<Producto> result = productoRepositorio.findAll();
+         if (result.isEmpty()){
+             return ResponseEntity.notFound().build();
+         }
+         return ResponseEntity.ok(result);
     }
 
     @GetMapping("api/producto/{id}")
-    public Producto obtenerUno(@PathVariable Long id){
-         return productoRepositorio.findById(id).orElse(null);
+    public ResponseEntity<?> obtenerUno(@PathVariable Long id){
+         Producto result = productoRepositorio.findById(id).orElse(null);
+         if (result==null)return ResponseEntity.notFound().build();
+         else return ResponseEntity.ok(result);
     }
 
     @PostMapping("api/producto")
