3e2c2e7777dbed188ca9115d03ae44ae8befe232
dam2 <mrubios>
2023-02-03 19:21:40 +0100

    Excepciones a la hora de hacer el HTTPStatus

---
+++ src/main/java/com/iesch/ad/rest/productos/rest/producto/error/ProductoNoEncontradoExcepcion.java
@@ -0,0 +1,11 @@
+package com.iesch.ad.rest.productos.rest.producto.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ProductoNoEncontradoExcepcion extends RuntimeException{
+    public ProductoNoEncontradoExcepcion(Long id){
+        super("No se ha podido encontrar ningún registro con ese parámetro " + id);
+    }
+}
